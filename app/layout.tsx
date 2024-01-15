fb8e68bd92f8dccdb9beb5653d8ca852edcf876a
Hung Ngo <hungngojan25@gmail.com>
2024-01-15 20:36:06 +0700

    add container for responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 import { HomeMeta } from '@/meta/Home';
+import NavBar from '@/components/navigation/NavBar';
+import Footer from '@/components/footer/Footer';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 const { title, description } = HomeMeta;
@@ -18,7 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={poppins.className}>{children}</body>
+      <body className={`${poppins.className} text-slate-700`}>
+        <div className='flex flex-col min-h-screen'>
+          <NavBar />
+          <main className='flex-grow bg-black'>{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
