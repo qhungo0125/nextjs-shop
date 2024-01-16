ab103bc691ad3cda8d85f5ec80c3d196feaf79e9
Hung Ngo <hungngojan25@gmail.com>
2024-01-16 11:00:11 +0700

    add product cell

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Container from '@/components/container/Container';
 import Banner from '@/components/homebanner/Banner';
+import ProductCard from '@/components/product/ProductCard';
+import { products } from '@/data/products';
+import { truncate } from '@/utils/text';
 import React from 'react';
 
 const Home = () => {
@@ -8,6 +11,11 @@ const Home = () => {
       <Container>
         <div>
           <Banner />
+        </div>
+        <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          {products.map((product) => (
+            <ProductCard key={product.name} product={product} />
+          ))}
         </div>
       </Container>
     </div>
---
+++ utils/text.tsx
@@ -0,0 +1,10 @@
+export const truncate = (str: String) => {
+  return str.length > 25 ? str.slice(0, 25) + '...' : str;
+};
+
+export const priceFormatter = (price: number) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(price);
+};
