173478914904c3bec8f1c16fe8157114da078f8e
Hung Ngo <hungngojan25@gmail.com>
2024-01-16 09:54:31 +0700

    add banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Container from '@/components/container/Container';
+import Banner from '@/components/homebanner/Banner';
+import React from 'react';
 
 const Home = () => {
-  return <div>E-shop</div>;
+  return (
+    <div className='p-8'>
+      <Container>
+        <div>
+          <Banner />
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
