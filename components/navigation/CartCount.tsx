3be8eabb5e4ce0678a243e3ae5f97eaab2305d18
qhungo0125 <hungngojan25@gmail.com>
2024-01-18 12:06:56 +0700

    add cart count

---
+++ components/navigation/CartCount.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { useCart } from '@/hooks/useCart';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { CiShoppingCart } from 'react-icons/ci';
+
+const CartCount = () => {
+  const { cartTotalQty } = useCart();
+  const router = useRouter();
+  return (
+    <div
+      onClick={() => router.push('/cart')}
+      className='relative cursor-pointer'
+    >
+      <div className='text-3xl'>
+        <CiShoppingCart size={24} />
+      </div>
+      <span className='absolute top-[-10px] right-[-10px] bg-slate-700 text-white w-6 h-6 rounded-full flex items-center justify-center text-sm'>
+        {cartTotalQty}
+      </span>
+    </div>
+  );
+};
+
+export default CartCount;
